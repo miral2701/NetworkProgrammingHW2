e5c415243f9ea07ddd4451507fa369a7f9f9acb7
Mira Madani <miramadani2701@gmail.com>
2024-08-20 16:41:05 +0300

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectToServer();
+            Console.ReadLine();
+        }
+        static void ConnectToServer()
+        {
+            TcpClient client=new TcpClient("127.0.0.1",8888);
+            Console.WriteLine("Connected to server");
+
+            NetworkStream stream =client.GetStream();
+            Console.WriteLine("Enter date -to get date \nEnter time=to get time");
+            string request=Console.ReadLine();
+            byte[] buffer = Encoding.UTF8.GetBytes(request);
+            stream.Write(buffer, 0, buffer.Length);
+          buffer=new byte[1024];
+
+            StringBuilder sb=new StringBuilder();
+            int bytes = stream.Read(buffer, 0, buffer.Length);
+            sb.Append(Encoding.UTF8.GetString(buffer,0,bytes));
+            Console.WriteLine("Response from server->"+sb.ToString());
+
+            client.Close();
+        }
+    }
+}
---
+++ NetworkProgrammingHW2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace NetworkProgrammingHW2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            TcpListener server = new TcpListener(ipAddress, 8888);
+            server.Start();
+            Console.WriteLine("Server started");
+            while(true)
+            {
+                TcpClient client= server.AcceptTcpClient();
+                Console.WriteLine("Client connected");
+                NetworkStream stream = client.GetStream();
+                byte[] buffer =new byte[1024];
+                int bytesRead =stream.Read(buffer, 0, buffer.Length);
+                string request=Encoding.UTF8.GetString(buffer,0,bytesRead);
+                Console.WriteLine("Request from client->"+request);
+
+                string response = String.Empty;
+                if (request.Equals("date", StringComparison.OrdinalIgnoreCase))
+                {
+                    response=DateTime.Now.ToShortDateString();
+                }
+                else if (request.Equals("time", StringComparison.OrdinalIgnoreCase))
+                {
+                    response = DateTime.Now.ToShortTimeString();
+
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                }
+                byte[] data=Encoding.UTF8.GetBytes(response);
+                stream.Write(data, 0, data.Length);
+                client.Close();
+            }
+        }
+    }
+}
